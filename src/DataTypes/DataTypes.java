891c488ee202fbbc9b0448c2dd95fa4e7c1d1993
Juan Galvis <galvisjuanc@gmail.com>
2023-05-15 09:33:45 -0500

    Data Types in Java

---
+++ src/DataTypes/DataTypes.java
@@ -0,0 +1,17 @@
+package DataTypes;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        int n = 1234567890;
+        long nL = 12345678901L;
+
+        double nD = 123.456;
+        float nF = 1234.567F;
+
+        System.out.println("Entero Integer: " + n);
+        System.out.println("Entero Long: " + nL);
+
+        System.out.println("Decimal Double: " + nD);
+        System.out.println("Decimal Flotante: " + nF);
+    }
+}
