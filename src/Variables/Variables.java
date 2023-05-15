2d2a270d4b9bb5d1912c9d9a87f212deb26f8c05
Juan Galvis <galvisjuanc@gmail.com>
2023-05-14 19:44:09 -0500

    Variables example

---
+++ src/Variables/Variables.java
@@ -0,0 +1,15 @@
+package Variables;
+
+public class Variables {
+    public static void main(String[] args) {
+
+        String cadena = "Tatakae SNK";
+        int velocidad = 60;
+        float aceleracion = 1.5f;
+        char letra = 'j';
+        boolean comida = true;
+
+        System.out.println(cadena + " " + velocidad + " " + aceleracion + " " + letra + " " + comida);
+
+    }
+}
