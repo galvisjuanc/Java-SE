31a22c29e38945bf23d0afb6b6b912e93529ee27
Juan Galvis <galvisjuanc@gmail.com>
2023-05-15 09:23:08 -0500

    lowerCamelCase example

--- src/NamingVariables/NamingVariables.java
+++ src/NamingVariables/NamingVariables.java
@@ -13,7 +13,12 @@ public class NamingVariables {
         String currency$ = "COP";
         String background_color = "Black";
 
-        System.out.println("País: " + $countryName + "y su moneda es :" + currency$);
-        System.out.println("Colores de fondo: " + _backgroundColor + background_color);
+        System.out.println("País: " + $countryName + " y su moneda es: " + currency$);
+        System.out.println("Colores de fondo: " + _backgroundColor + " y " + background_color);
+
+        //Lower Camel Case
+        String fullName = "Juan Camilo Galvis Cuéllar";
+        int sizeInCentimeters = 180;
+        System.out.println("Nombre: " + fullName + " y su tamaño en centimetros es: " + sizeInCentimeters);
     }
 }
