7afb96cf6eff91c875c4cef6a5bd1d298bcf86a7
Juan Galvis <galvisjuanc@gmail.com>
2023-05-19 10:14:16 -0500

    Examen Java SE

---
+++ src/examen/examen.java
@@ -0,0 +1,19 @@
+package examen;
+
+public class examen {
+    public static void main(String[] args) {
+        int i = 7;
+        char c = 'w';
+        System.out.println((i>=6) && (c == 'w'));
+
+        int x = 1;
+        while(x <=10) {
+            System.out.println(++x);
+        }
+
+        int a = 1, b = 2, d = 3, f = 2;
+        System.out.println((b >= a) || (d == f));
+
+        System.out.println('j' + 'a' + 'v' + 'a');
+    }
+}
