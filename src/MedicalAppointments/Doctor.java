1af456c9dde36db4c462338e08d926107a33353e
Juan Galvis <galvisjuanc@gmail.com>
2023-05-23 07:36:29 -0500

    Metodo constructor

--- src/MedicalAppointments/Doctor.java
+++ src/MedicalAppointments/Doctor.java
@@ -5,8 +5,16 @@ public class Doctor {
     String name;
     String speciality;
 
+    Doctor() {
+        System.out.println("Construyendo el Objeto Doctor");
+    }
+
+    Doctor(String name) {
+        System.out.println("Name's Doctor: " + name);
+    }
+
     //Comportamientos
     public void showName() {
-        System.out.println(name);
+        System.out.println("Mostrando el nombre: " + name);
     }
 }
