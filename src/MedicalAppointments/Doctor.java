253f5ed0c3425dfe9ff2c5a6153a99ea4cc37ca5
Juan Galvis <galvisjuanc@gmail.com>
2023-05-23 07:27:00 -0500

    Medical Appointments Project began

---
+++ src/MedicalAppointments/Doctor.java
@@ -0,0 +1,12 @@
+package MedicalAppointments;
+
+public class Doctor {
+    int id;
+    String name;
+    String speciality;
+
+    //Comportamientos
+    public void showName() {
+        System.out.println(name);
+    }
+}
