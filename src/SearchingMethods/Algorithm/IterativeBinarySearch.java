6aaa2e16f58822ca8b0feebee7805251a3fa2cc0
Juan Galvis <galvisjuanc@gmail.com>
2023-07-18 07:12:56 -0500

    IterativeBinary Search

---
+++ src/SearchingMethods/Algorithm/IterativeBinarySearch.java
@@ -0,0 +1,21 @@
+package SearchingMethods.Algorithm;
+
+
+/**
+ * Binary search is one of the most popular algorithms This class represents
+ * iterative version {@link BinarySearch} Iterative binary search is likely to
+ * have lower constant factors because it doesn't involve the overhead of
+ * manipulating the call stack. But in java the recursive version can be
+ * optimized by the compiler to this version.
+ *
+ * <p>
+ * Worst-case performance O(log n) Best-case performance O(1) Average
+ * performance O(log n) Worst-case space complexity O(1)
+ *
+ * @author Gabriele La Greca : https://github.com/thegabriele97
+ * @author Podshivalov Nikita (https://github.com/nikitap492)
+ * @see SearchAlgorithm
+ * @see BinarySearch
+ */
+public class IterativeBinarySearch {
+}
