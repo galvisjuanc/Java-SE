4434ed2e8436a7fb9e422645758afc69e5518a51
Juan Galvis <galvisjuanc@gmail.com>
2023-09-01 07:10:32 -0500

    Clase Abstracta Poligono

---
+++ src/POO/Ejercicios/Ej10/Poligono.java
@@ -0,0 +1,28 @@
+package POO.Ejercicios.Ej10;
+
+public abstract class Poligono {
+
+        protected int numeroLados;
+
+        public Poligono(int numeroLados) {
+            this.numeroLados = numeroLados;
+        }
+
+        public int getNumeroLados() {
+            return numeroLados;
+        }
+
+        public void setNumeroLados(int numeroLados) {
+            this.numeroLados = numeroLados;
+        }
+
+        @Override
+        public String toString() {
+            return "Poligono{" +
+                    "Número Lados=" + numeroLados +
+                    '}';
+        }
+
+        public abstract double area();
+
+}
