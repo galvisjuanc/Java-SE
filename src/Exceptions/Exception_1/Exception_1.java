d40364ed072233eb1f96ed187ea9d0fe187e7908
Juan Galvis <galvisjuanc@gmail.com>
2023-09-25 09:00:52 -0500

    Tweak 2

--- src/Exceptions/Exception_1/Exception_1.java
+++ src/Exceptions/Exception_1/Exception_1.java
@@ -12,6 +12,7 @@ public class Exception_1 {
 
         System.out.println("El resultado es: "+resultado);
         // La división por cero genera un ArithmeticException. El programa se detiene en la operación de resultado
+        // porque es una división no correcta.
         */
         int numero;
         Scanner entrada = new Scanner(System.in);
