5947ae245a9deb960b2a0a4df6156f8fc59c2dbd
Juan Galvis <galvisjuanc@gmail.com>
2023-09-28 10:08:20 -0500

    Code  for Exception 5

--- src/Exceptions/Exception_5/Exception_5.java
+++ src/Exceptions/Exception_5/Exception_5.java
@@ -1,4 +1,29 @@
 package Exceptions.Exception_5;
 
+import javax.swing.*;
+
 public class Exception_5 {
+    public void operaciones(){
+        int num1 = 2;
+        int num2 = 0;
+        int resultado;
+
+        resultado = num1 / num2;
+        System.out.println("El resultado es: "+resultado);
+    }
+
+    public void operaciones2(){
+        try{
+            operaciones();
+        } catch (ArithmeticException a){
+            JOptionPane.showMessageDialog(null, "Ha intentado dividir un número entero por cero. Eso es un error");
+        }
+        System.out.println("\nPrograma Terminado");
+    }
+
+    public static void main(String[] args) {
+        Exception_5 ex5 = new Exception_5();
+
+        ex5.operaciones2();
+    }
 }
