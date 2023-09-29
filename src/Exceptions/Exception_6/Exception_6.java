d725a915d66da0c1340ee778a3c32fd0d82fc371
Juan Galvis <galvisjuanc@gmail.com>
2023-09-29 11:15:21 -0500

    Customized Exception in the main class

--- src/Exceptions/Exception_6/Exception_6.java
+++ src/Exceptions/Exception_6/Exception_6.java
@@ -1,9 +1,36 @@
 package Exceptions.Exception_6;
 
 
+import java.util.Scanner;
+
 /**
  * Uso de throw para excepciones personalizada
  * */
 public class Exception_6 {
+    private int numero;
+    private Scanner entrada;
 
+    public void introducirNumeros() throws CeroException {
+        entrada = new Scanner(System.in);
+
+        do{
+            System.out.print("Digite un número: ");
+            numero = entrada.nextInt();
+
+            if(numero == 0){
+                throw new CeroException();
+            }
+        }while(numero != 0 && numero != 1);
+    }
+
+    public static void main(String[] args){
+        Exception_6 ex6 = new Exception_6();
+        try {
+            ex6.introducirNumeros();
+        } catch (CeroException e) {
+            System.out.println("Ha introducido el cero. Error fatal! ---> " + e);
+        }
+
+        System.out.println("Programa terminado.");
+    }
 }
