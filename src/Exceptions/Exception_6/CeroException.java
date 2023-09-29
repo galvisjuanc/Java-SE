5f9aa1a930f65997fea1e2c0b951976cc2a6bfb2
Juan Galvis <galvisjuanc@gmail.com>
2023-09-29 11:16:56 -0500

    Tweak 2

--- src/Exceptions/Exception_6/CeroException.java
+++ src/Exceptions/Exception_6/CeroException.java
@@ -3,6 +3,6 @@ package Exceptions.Exception_6;
 public class CeroException extends Exception {
 
     public CeroException(){
-        super("Se ha introducido el número 0");
+        super("Se ha introducido el número 0 y eso ha causado un error");
     }
 }
