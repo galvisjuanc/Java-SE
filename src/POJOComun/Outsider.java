3297b106bdf1bd02dbacd67eb1c4ffab7594be4b
Juan Galvis <galvisjuanc@gmail.com>
2023-11-06 17:02:43 -0500

    Mutable Classes

--- src/POJOComun/Outsider.java
+++ src/POJOComun/Outsider.java
@@ -13,5 +13,70 @@ public class Outsider {
         sier.setFirstName("Israel");
         sier.setLastName("Sergio");
 
+        //Peligros de una clase mutable
+        System.out.println(sier);
+        badFunction(sier);
+        System.out.println(sier);
+
+        System.out.println("//////////////////////////////");
+        MutablePerson_2 sinuhe = new MutablePerson_2(sierEmail);
+        System.out.println(sinuhe);
+        otherBadFunction(sinuhe);
+        System.out.println(sinuhe);
+
+        System.out.println("///////////////////////////////");
+
+
+        MutablePerson_3 sinuhe_3 = new MutablePerson_3(sierEmail);
+        System.out.println(sinuhe_3);
+        otherBadFunctionPart3(sinuhe_3);
+        System.out.println(sinuhe_3);
+
+
+
+
+
+        System.out.println("///////////////////////////////");
+
+
+
+
+
+        MutablePerson_3 sinuhe_4 = new MutablePerson_4(sierEmail);
+        System.out.println(sinuhe_4);
+    }
+
+    /**
+     * Este metodo modifica la lista mediante un setter.
+     * Tener el setter es peligroso…
+     */
+    static void badFunction(MutablePerson person) {
+        List<String> emails = new LinkedList<>();
+        emails.add("imnotevil@mail.com");
+
+        person.setEmails(emails);
+    }
+
+    /**
+     * Este metodo toma el objeto devuelto por el getter… pero el
+     * objeto es mutable, asi que podemos modificarlo sin restricciones…
+     */
+    static void otherBadFunction(MutablePerson_2 person) {
+        List<String> emails = person.getEmails();
+        emails.clear();
+
+        emails.add("imnotevil@mail.com");
+    }
+
+    static void otherBadFunctionPart3(MutablePerson_3 person) {
+        List<String> spammyEmails = new LinkedList<>();
+        spammyEmails.add("tubanco@mibanco.banco.com");
+        spammyEmails.add("cheapfoods@blackmarket.com");
+
+        List<String> emails = person.getEmails();
+        emails.clear();
+
+        emails.add("imnotevil@mail.com");
+        emails.addAll(spammyEmails);
     }
 }
